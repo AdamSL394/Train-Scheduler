956b323ac81805186c9d3988026703af66d696f1
Adam <lehreradam@yahoo.com>
2019-06-06 19:53:05 -0700

    linked to all files css & java & firebase

---
+++ assets/game.js
@@ -0,0 +1,15 @@
+//
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDQJgZVr0MWhKgHKWuzAS7jCT45U-2Wm84",
+    authDomain: "adam-546e1.firebaseapp.com",
+    databaseURL: "https://adam-546e1.firebaseio.com",
+    projectId: "adam-546e1",
+    storageBucket: "adam-546e1.appspot.com",
+    messagingSenderId: "153073052495",
+    appId: "1:153073052495:web:eff5d58f3579f56a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
