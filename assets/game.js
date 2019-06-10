71bfdfde3732038f1e00e53a01203766ec328132
Adam <lehreradam@yahoo.com>
2019-06-10 16:57:41 -0700

    minutes away and next train time works

--- assets/game.js
+++ assets/game.js
@@ -14,6 +14,8 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 console.log(database);
 
+
+
 // can I write on document on click of anything with the class of btn run this function, which is better
 $(".btn").on("click",function(){
     
@@ -21,16 +23,39 @@ $(".btn").on("click",function(){
     var destinationInput=$("#destinationInput").val().trim();
     var firstTrainTime= $("#firstTrainTime").val().trim();
     var frequency=$("#frequency").val().trim();
+    console.log("below is the frqeuency and type")
+  console.log(frequency)
 
-    var trainDetail= moment(firstTrainTime,"HH:mm").subtract(1,"years");
+   var parsedNum=parseInt(frequency);
+
+    console.log("this is parsed Num"+ parsedNum);
+
+    var trainDetail= moment(firstTrainTime,"HH:mm").subtract(1, "years");
+    nextTrain=trainDetail
     console.log(trainDetail);
+  // console.log("this is the NEXT TRAIN "+ nextTrain)
+
 
     var currentTime =moment();
     console.log("Current Time: " + moment(currentTime).format("hh:mm"));
 
-    
+
+      var diffTime = moment().diff(moment(nextTrain),
+      'minutes');
+      console.log("this is diff train time:", diffTime);
 
 
+    var timeRemainder =   diffTime % parsedNum
+    console.log("this is the time remainder"+ timeRemainder)
+
+    var minutesTillTrain= parsedNum - timeRemainder;
+console.log("MINUTES TILL TRAIN: " + minutesTillTrain);
+
+  var nextTrainTime= moment().add(minutesTillTrain,"minutes");
+  console.log("____________")
+  console.log(moment().add(minutesTillTrain,"minutes"))
+  console.log("ARRIVAL TIME: " + moment(nextTrainTime).format("hh:mm"));
+  var t = moment(nextTrainTime).format("hh:mm");
 
 
 
@@ -38,30 +63,29 @@ $(".btn").on("click",function(){
       trainNameInput:trainNameInput,
         destinationInput:destinationInput,
         firstTrainTime:firstTrainTime,
-        frequency:frequency
+        frequency:frequency,
+        nextTrainTime:t,
+        minutesTillTrain:minutesTillTrain,
+        
+        
+
+        
     })
   })
   
   database.ref().on("child_added",function(childsnapshot){
   console.log(childsnapshot.val().trainNameInput);
   console.log(childsnapshot.val().destinationInput);
-  console.log(childsnapshot.val().firstTrainTime);
+  // console.log(childsnapshot.val().firstTrainTime);
   console.log(childsnapshot.val().frequency);
+  console.log(childsnapshot.val().nextTrainTime);
+  console.log(childsnapshot.val().minutesTillTrain);
+
+  $("#trainTable").append("<tr><td class='move'> " + childsnapshot.val().trainNameInput + "</td>"+ "<hr>" + "<td class='move'>"+ childsnapshot.val().destinationInput+ "</td>" + " <td class='move'>" + childsnapshot.val().frequency + "</td>" + "<td class='move'>" + childsnapshot.val().nextTrainTime + "</td> " + "<td>"+ childsnapshot.val().minutesTillTrain + "</td> </tr>");
+
+ 
 
 
-
-  $("#trainTable").append("<tr><td class='move'> " + childsnapshot.val().trainNameInput + "</td>"+ "<hr>" + "<td class='move'>"+ childsnapshot.val().destinationInput+ "</td>" + " <td class='move'>" + childsnapshot.val().firstTrainTime + "</td>" + "<td class='move'>" + childsnapshot.val().frequency + "</td> </tr>");
   
 
   })
-  // console.log(firstTrainTime.diff(moment(), "months"));
-  // console.log(firstTrainTime.diff(moment(), "days"));
-  // console.log(firstTrainTime.diff(moment(), "weeks"));
-  // console.log(firstTrainTime.diff(moment(), "hours"));
-  // console.log(firstTrainTime.diff(moment(), "minutes"));
-
-
-  
-
-
-// });
