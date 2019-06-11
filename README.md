733f8144a9476d69dcd768c65f977d7eeca0ed48
Adam <lehreradam@yahoo.com>
2019-06-10 17:46:39 -0700

    read me added

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Train-Scheduler-
+# Train-Scheduler
+This repo shows the use of Javascript with Firebase to store data. When the page is loaded we have stored details of train times that are pulled from Firebase and appended to the table. We are grabbing these values the user inputs and storing them in an object with a key/value pair in Firebase, taking a snapshot each time the information is added too. Furthermore we are passing the First Train time through the moment method and calculating the minutes away and time arrival. We do this by passing the train time the user inputs into the moment function where the computer registers the current time as an object in hour and minute format. We must also take the frequency of the train and put that into number. Next we take the current time given to us by the moment function library and convert that and the arrival of the train time all into minutes. We take the remainder known and store it as a variable diffTime.  By taking the current time in minutes and the difference of the train arrival time in minutes we find the remainder of frequency divided into that total number.This subtracted by the frequency give us the minutes remaining until the next train arrives. Finally we must add the current time and the minutes till the next train arrive to give us the time that the train arrives. 
+
+## Perquisites
+You need to git pull this repo to your desktop using git clone . and the url from git hub. 
+
+## Running Tests/Instructions
+Open the file in  your browser (preferably google chrome) to test the code.
+This will show a table with stored data loaded from Firebase of Train Names, the destination, frequency, next arrival, and minutes away. The user can input their own train information and the program will calculate the next time the train will arrive how many minutes it is away from arriving. 
+
+## Built with:
+<ol>
+<li> HTML 
+<li> CSS 
+<li> Javascript
+<li> JQuery
+<li> Firebase
+<li> Moment.js
+</ol>
+
+### Local Development Environment for website Repo
+The following will get up and running locally.
+
+Author
+Adam Lehrer
+
+![Train Scheduler]()
+
--- assets/game.js
+++ assets/game.js
@@ -1,91 +1,97 @@
+$(document).ready(function () {
+
   // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyDQJgZVr0MWhKgHKWuzAS7jCT45U-2Wm84",
-  authDomain: "adam-546e1.firebaseapp.com",
-  databaseURL: "https://adam-546e1.firebaseio.com",
-  projectId: "adam-546e1",
-  storageBucket: "adam-546e1.appspot.com",
-  messagingSenderId: "153073052495",
-  appId: "1:153073052495:web:eff5d58f3579f56a"
-};
+  var firebaseConfig = {
+    apiKey: "AIzaSyDQJgZVr0MWhKgHKWuzAS7jCT45U-2Wm84",
+    authDomain: "adam-546e1.firebaseapp.com",
+    databaseURL: "https://adam-546e1.firebaseio.com",
+    projectId: "adam-546e1",
+    storageBucket: "adam-546e1.appspot.com",
+    messagingSenderId: "153073052495",
+    appId: "1:153073052495:web:eff5d58f3579f56a"
+  };
   // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
-var database = firebase.database();
-console.log(database);
+  var database = firebase.database();
+  console.log(database);
 
 
 
-// can I write on document on click of anything with the class of btn run this function, which is better
-$(".btn").on("click",function(){
-    
-    var trainNameInput=$("#trainNameInput").val().trim();
-    var destinationInput=$("#destinationInput").val().trim();
-    var firstTrainTime= $("#firstTrainTime").val().trim();
-    var frequency=$("#frequency").val().trim();
-    console.log("below is the frqeuency and type")
-  console.log(frequency)
+  // can I write on document on click of anything with the class of btn run this function, which is better
+  $(".btn").on("click", function () {
+    // event.preventDefault();
 
-   var parsedNum=parseInt(frequency);
+    var trainNameInput = $("#trainNameInput").val().trim();
+    var destinationInput = $("#destinationInput").val().trim();
+    var firstTrainTime = $("#firstTrainTime").val().trim();
+    var frequency = $("#frequency").val().trim();
+    console.log("below is the frequency and type")
+    console.log(frequency)
 
-    console.log("this is parsed Num"+ parsedNum);
+    var parsedNum = parseInt(frequency);
 
-    var trainDetail= moment(firstTrainTime,"HH:mm").subtract(1, "years");
-    nextTrain=trainDetail
+    console.log("this is parsed Num" + parsedNum);
+
+    var trainDetail = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+    nextTrain = trainDetail
     console.log(trainDetail);
-  // console.log("this is the NEXT TRAIN "+ nextTrain)
+    // console.log("this is the NEXT TRAIN "+ nextTrain)
 
 
-    var currentTime =moment();
+    var currentTime = moment();
     console.log("Current Time: " + moment(currentTime).format("hh:mm"));
 
 
-      var diffTime = moment().diff(moment(nextTrain),
+    var diffTime = moment().diff(moment(nextTrain),
       'minutes');
-      console.log("this is diff train time:", diffTime);
+    console.log("this is diff train time:", diffTime);
 
 
-    var timeRemainder =   diffTime % parsedNum
-    console.log("this is the time remainder"+ timeRemainder)
+    var timeRemainder = diffTime % parsedNum
+    console.log("this is the time remainder" + timeRemainder)
 
-    var minutesTillTrain= parsedNum - timeRemainder;
-console.log("MINUTES TILL TRAIN: " + minutesTillTrain);
+    var minutesTillTrain = parsedNum - timeRemainder;
+    console.log("MINUTES TILL TRAIN: " + minutesTillTrain);
 
-  var nextTrainTime= moment().add(minutesTillTrain,"minutes");
-  console.log("____________")
-  console.log(moment().add(minutesTillTrain,"minutes"))
-  console.log("ARRIVAL TIME: " + moment(nextTrainTime).format("hh:mm"));
-  var t = moment(nextTrainTime).format("hh:mm");
+    var nextTrainTime = moment().add(minutesTillTrain, "minutes");
+    console.log("____________")
+    console.log(moment().add(minutesTillTrain, "minutes"))
+    console.log("ARRIVAL TIME: " + moment(nextTrainTime).format("hh:mm"));
+    var t = moment(nextTrainTime).format("hh:mm");
 
 
 
     database.ref().push({
-      trainNameInput:trainNameInput,
-        destinationInput:destinationInput,
-        firstTrainTime:firstTrainTime,
-        frequency:frequency,
-        nextTrainTime:t,
-        minutesTillTrain:minutesTillTrain,
-        
-        
+      trainNameInput: trainNameInput,
+      destinationInput: destinationInput,
+      firstTrainTime: firstTrainTime,
+      frequency: frequency,
+      nextTrainTime: t,
+      minutesTillTrain: minutesTillTrain,
 
-        
+
+
+
     })
   })
-  
-  database.ref().on("child_added",function(childsnapshot){
-  console.log(childsnapshot.val().trainNameInput);
-  console.log(childsnapshot.val().destinationInput);
-  // console.log(childsnapshot.val().firstTrainTime);
-  console.log(childsnapshot.val().frequency);
-  console.log(childsnapshot.val().nextTrainTime);
-  console.log(childsnapshot.val().minutesTillTrain);
 
-  $("#trainTable").append("<tr><td class='move'> " + childsnapshot.val().trainNameInput + "</td>"+ "<hr>" + "<td class='move'>"+ childsnapshot.val().destinationInput+ "</td>" + " <td class='move'>" + childsnapshot.val().frequency + "</td>" + "<td class='move'>" + childsnapshot.val().nextTrainTime + "</td> " + "<td>"+ childsnapshot.val().minutesTillTrain + "</td> </tr>");
-
- 
+  database.ref().on("child_added", function (childsnapshot) {
+    console.log(childsnapshot.val().trainNameInput);
+    console.log(childsnapshot.val().destinationInput);
+    // console.log(childsnapshot.val().firstTrainTime);
+    console.log(childsnapshot.val().frequency);
+    console.log(childsnapshot.val().nextTrainTime);
+    console.log(childsnapshot.val().minutesTillTrain);
 
 
-  
+
+    $("#trainTable").append("<tr><td class='move'> " + childsnapshot.val().trainNameInput + "</td>" + "<hr>" + "<td class='move'>" + childsnapshot.val().destinationInput + "</td>" + " <td class='move'>" + childsnapshot.val().frequency + "</td>" + "<td class='move'>" + childsnapshot.val().nextTrainTime + "</td> " + "<td>" + childsnapshot.val().minutesTillTrain + "</td> </tr>");
+
+
+
+
 
   })
+
+})
